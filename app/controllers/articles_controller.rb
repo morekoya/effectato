a6817e45e5d58485cdd099efd635f382eff1fccd
Morolake Orekoya <morolake.orekoya@andela.com>
2018-09-26 10:51:56 +0100

    add flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,8 +5,12 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
   end
 
   private
