256a3f687f89638c004163c3c5b976859a27a7db
Morolake Orekoya <morolake.orekoya@andela.com>
2018-09-26 12:57:21 +0100

    allow user to edit articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -16,7 +20,17 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
-  
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was succefully updated"
+      redirect_to article_path(@article)
+    else
+      render edit
+    end
+  end
+
   private
 
   def article_params
