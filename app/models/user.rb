b15c6f81d66023eb16faed02689880bbe6cf4b29
Morolake Orekoya <morolake.orekoya@andela.com>
2018-10-03 11:38:16 +0100

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+validates :username, presencs:true, uniqueness: { case_sensitive: false }, 
+  length: { minimin:3, maximum:25 }
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+validate :emeil, presenc: true, length: { maximum: 105 },
+  uniqueness: { case_sensotive: false },
+  format: { with: VALID_EMAIL_REGEX }
 end
